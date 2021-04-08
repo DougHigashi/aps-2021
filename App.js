4e184a835e45bc7d8268db0e9180be8e817a0871
Doug <higoshi.douglas@gmail.com>
2021-04-07 22:02:37 -0300

    Tela de login pronta

--- App.js
+++ App.js
@@ -1,21 +1,77 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+
+  const [form, setForm] = useState({
+    login: "",
+    password: ""
+  });
+
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.logo}>Faça Login</Text>
+      <TextInput placeholder="Login" style={styles.input} />
+      <TextInput placeholder="Senha" style={styles.input} />
+
+      <TouchableOpacity>
+        <Text style={styles.esqueciSenha}>Esqueci minha senha</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.loginBtn}>
+        <Text style={styles.loginText}>LOGIN</Text>
+      </TouchableOpacity>
+
       <StatusBar style="auto" />
-    </View>
+    </View >
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#003f5c',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  logo: {
+    fontWeight: 'bold',
+    fontSize: 30,
+    marginBottom: 40,
+    color: "#fb5b5a",
+  },
+
+  input: {
+    width: '70%',
+    marginBottom: 20,
+    padding: 10,
+    height: 50,
+    backgroundColor: '#465881',
+    borderRadius: 25,
+    justifyContent: 'center',
+  },
+
+  esqueciSenha: {
+    color: 'white',
+  },
+
+  loginBtn: {
+    width: '70%',
+    backgroundColor: '#fb5b5a',
+    height: 50,
+    marginTop: 40,
+    marginBottom: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 25
+  },
+
+  loginText: {
+    color: 'white'
+  }
 });

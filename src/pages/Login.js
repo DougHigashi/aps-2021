0ab8d4643ee2d81fc07484c1e0bb5c3fe259b7a4
Doug <higoshi.douglas@gmail.com>
2021-04-21 18:54:47 -0300

    Adicionando README.MD e corrigindo erros nas telas de login e cadastro

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,35 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
-import firebase from 'firebase';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View, Alert } from 'react-native';
+import firebase from '../config/Firebase';
 
 export default function App({ navigation }) {
 
     const [login, setLogin] = useState('');
     const [password, setPassword] = useState('');
-    
-    
-    function authentication(){
-        firebase.auth().signInWithEmailAndPassword(login, password)
-        .then(()=>navigation.navigate('Chat')
-        ).cath(function (error){
-            var errorCode = error.Code;
-            var errorMessage = error.Message;
-            alert(errorCode, errorMessage);
-        });
-    }
 
+
+    function authentication() {
+
+        firebase.auth().signInWithEmailAndPassword(login, password)
+            .then(() => navigation.navigate('Chat')
+            ).catch((error) => {
+                console.log(error)
+                Alert.alert("Ops!", error.message);
+            });
+    }
 
     return (
         <View style={styles.container}>
             <Text style={styles.logo}>Faça Login</Text>
-            <TextInput placeholder="Login" style={styles.input} onChangeText={login =>setLogin(login)} value={login}/>
-            <TextInput placeholder="Senha" style={styles.input} onChangeText={password =>setPassword(password)} value={password}/>
-            
-            <TouchableOpacity onPress={()=>{authentication()}} style={styles.loginBtn}>
+            <TextInput placeholder="Login" style={styles.input} onChangeText={login => setLogin(login)} value={login} />
+            <TextInput placeholder="Senha" style={styles.input} secureTextEntry={true} onChangeText={password => setPassword(password)} value={password} />
+
+            <TouchableOpacity onPress={() => { authentication() }} style={styles.loginBtn}>
                 <Text style={styles.loginText}>LOGIN</Text>
             </TouchableOpacity>
-            
+
             <TouchableOpacity onPress={() => navigation.navigate('Cadastro')}>
                 <Text style={styles.esqueciSenha}>Se cadastrar</Text>
             </TouchableOpacity>
@@ -47,14 +46,12 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
-
     logo: {
         fontWeight: 'bold',
         fontSize: 30,
         marginBottom: 40,
         color: "#fb5b5a",
     },
-
     input: {
         width: '70%',
         marginBottom: 20,
@@ -64,11 +61,9 @@ const styles = StyleSheet.create({
         borderRadius: 25,
         justifyContent: 'center',
     },
-
     esqueciSenha: {
         color: 'white',
     },
-
     loginBtn: {
         width: '70%',
         backgroundColor: '#fb5b5a',
@@ -79,7 +74,6 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         borderRadius: 25
     },
-
     loginText: {
         color: 'white'
     }
